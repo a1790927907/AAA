9149e6b8fe93046cb2156a3a7cda34be98563ebf
a1790927907 <1790927907@qq.com>
2020-10-25 07:21:47 +0800

    settings

---
+++ sp_requests/settings.py
@@ -0,0 +1,33 @@
+#redis配置
+configs = {
+    'host':'localhost',
+    'password':'0365241lk',
+    'port':6379,
+}
+
+#用于读取关键词的文件
+#关键词用逗号隔开，无须换行
+#若无，则不写
+file_path = ''
+
+#请求间隔
+DOWNLOAD_DELAY = 1
+
+#每次处理多少数据
+DATA_MANAGE_NUM = 16
+
+#mysql配置
+MYSQL_USERNAME = 'root'
+MYSQL_PWD = '0365241lk'
+MYSQL_DATABASE = 'baidu'
+
+#可根据CPU核心数设定进程数量
+#若不设定即无限
+MAX_PROCESS_NUM = 2
+
+
+
+
+
+
+
